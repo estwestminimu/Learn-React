18035475297d21f69bc6d5316dfd3e3e7d328b53
Szymon <96234081+estwestminimu@users.noreply.github.com>
2022-06-18 21:51:07 +0200

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  render(
+  return (
     <div>
       <h1>Adopt me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
